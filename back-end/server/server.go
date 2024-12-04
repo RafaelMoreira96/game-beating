4b8e6d327bd6f9fc778ebeb65250b605ce66ee89
Rafael Moreira <rafaelmoreira.at@hotmail.com>
2024-12-03 20:21:52 -0400

    fix: add a new url for cors from API

--- back-end/server/server.go
+++ back-end/server/server.go
@@ -14,7 +14,7 @@ func RunServer() {
 
 	app.Use(helmet.New())
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:4200",
+		AllowOrigins: "http://localhost:4200, https://game-beating.vercel.app/",
 		AllowMethods: "GET, POST, PUT, DELETE, OPTIONS",
 		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
 	}))
